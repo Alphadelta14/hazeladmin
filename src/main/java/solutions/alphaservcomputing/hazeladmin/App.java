1589d9bd717fc61a6e17e83a21a14d25b29c237f
Alpha <alpha@pokesplash.net>
2016-09-15 18:52:00 -0400

    Initialized template Java repo

---
+++ src/main/java/solutions/alphaservcomputing/hazeladmin/App.java
@@ -0,0 +1,13 @@
+package solutions.alphaservcomputing.hazeladmin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
